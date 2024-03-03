f142ac323e6bd8f6a958039fc2544ea8506bda43
sanjar29 <sanjar29@colostate.edu>
2024-03-02 23:31:08 -0700

    added functional equations

--- equations.py
+++ equations.py
@@ -7,9 +7,9 @@ from isConnected import is_connected
 from latency import check_latency 
 
 def get_final_stats():
-    uni_statistic = 0
-    if(is_connected()): 
-
+    if(is_connected()):
+         
+        list = []
         #print("ispStatus returned: ", end = "")
         internet_provider_status_stat = get_statistic_ispStatus()
 
@@ -29,57 +29,73 @@ def get_final_stats():
         #print("get_statistic_timeLost returned: ", end = "")
         lost_time_stat = get_statistic_lostTime()
 
-        # print("SANJAR")
-        # print(internet_provider_status_stat)
-        # print(router_light_up_stat)
-        # print(power_supply_stat)
-        # print(latency_stat)
-        # print(lost_package_stat)
-        # print(lost_time_stat)
-
+        print("SANJAR")
+        print(internet_provider_status_stat)
+        print(router_light_up_stat)
+        print(power_supply_stat)
+        print(latency_stat)
+        print(lost_package_stat)
+        print(lost_time_stat)
+        
+        
+        
+        # STOP HERE
         if internet_provider_status_stat == 1:
-            return 100
+            return [100, 1]
+        else:
+            list.append(0)
+
         
         if(router_light_up_stat == 1):
-            return 100
+            return [100, 1]
+        else:
+            list.append(0)
         if(power_supply_stat == 1):
-            return 100
-        
+            return [100, 1]
+        else:
+            list.append(0)
 
         if(latency_stat == "very_bad"):
-            return 100
+            return [100,1]
         elif(latency_stat == "bad"):
-            return 70
-        elif(latency_stat == "good"):
-            return 30
+            list.append(70)
+        elif(latency_stat == "fine"):
+            list.append(30)
         elif(latency_stat == "very_good"):
-            return 0
+            list.append(0)
+        else:
+            list.append(0)
 
         if(lost_package_stat >= 0.75):
-            return 100
+            return [100,1]
         elif (lost_package_stat >= 0.45):
-            return 75
+            list.append(75)
         elif (lost_package_stat >= 0.2):
-            return 20
+            list.append(20)
         elif (lost_package_stat == 0):
-            return 0
+            list.append(0)
+        else:
+            list.append(0)
 
 
         if(lost_time_stat == 1):
-            return 100
+            return [100,1]
         elif lost_time_stat == 0.75:
-            return 75
+            list.append(75)
         elif lost_time_stat == 0.5:
-            return 50
+            list.append(50)
         elif lost_time_stat == 0.3:
-            return 30
+            list.append(30)
         elif lost_time_stat == 0:
-            return 0
-        
-        return uni_statistic
-        
-        
+            list.append(0)
+        else:
+            list.append(0)
 
+        list = sorted(list, reverse = True)
+        temp = ((list[0] + list[1]*(0.5)+ list[2]*(0.25)))
+        temp  =  ((100) if (temp > 100) else (temp))
+        print("DELETE THIS AFTER")
+        return [temp, temp/100]
+    
     else:
-        return 100
-
+        return [100,1]
