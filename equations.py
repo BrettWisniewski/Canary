1cf3aca0cf36f4ed55d1b6d53d843c208c7f4d34
sanjar29 <sanjar29@colostate.edu>
2024-03-03 09:02:56 -0700

    Done hopefully

--- equations.py
+++ equations.py
@@ -4,59 +4,84 @@ from power_supply import  get_statistic_powerSupply
 from lostPackets import get_statistic_lostPackage
 from timeLost import get_statistic_lostTime
 from isConnected import is_connected
-from latency import check_latency 
+from latency import check_latency
+import collections
+
+
 
 def get_final_stats():
+    dictionaryAsArgument = {}
     if(is_connected()):
-         
+
         list = []
         #print("ispStatus returned: ", end = "")
-        internet_provider_status_stat = get_statistic_ispStatus()
+        internet_provider_status_stat_list = get_statistic_ispStatus()
+        internet_provider_status_stat = internet_provider_status_stat_list[0]
+        internet_provider_status_stat_status = internet_provider_status_stat_list[1]
+
+        dictionaryAsArgument['Internet Service Provider'] = internet_provider_status_stat_status
 
         #print("ispStatus returned: ", end = "")
         latency_stat = check_latency()
         
         
         #print("routerLightsUp returned: ", end = "")
-        router_light_up_stat = get_statistic_routerLightsUp()
+        router_light_up_stat_list = get_statistic_routerLightsUp()
+        router_light_up_stat = router_light_up_stat_list[0]
+        router_light_up_stat_status = router_light_up_stat_list[1]
+        dictionaryAsArgument['Router Light'] = router_light_up_stat_status
+
 
         #print("get_statistic_powerSupply returned: ", end = "")
-        power_supply_stat = get_statistic_powerSupply()
+        power_supply_stat_list = get_statistic_powerSupply()
+        power_supply_stat = power_supply_stat_list[0]
+        power_supply_stat_status =  power_supply_stat_list[1]
+
+        dictionaryAsArgument['Power supply'] = power_supply_stat_status
 
         #print("get_statistic_lostPackage returned: ", end = "")
-        lost_package_stat = get_statistic_lostPackage()
+        lost_package_stat_list = get_statistic_lostPackage()
+        lost_package_stat = lost_package_stat_list[0]
+        lost_package_stat_status = lost_package_stat_list[1]
+
+        dictionaryAsArgument['Lost Packages'] = lost_package_stat_status
+
 
         #print("get_statistic_timeLost returned: ", end = "")
-        lost_time_stat = get_statistic_lostTime()
+        lost_time_stat_list = get_statistic_lostTime()
+        lost_time_stat = lost_time_stat_list[0]
+        lost_time_stat_status = lost_time_stat_list[1]
 
-        print("SANJAR")
-        print(internet_provider_status_stat)
-        print(router_light_up_stat)
-        print(power_supply_stat)
-        print(latency_stat)
-        print(lost_package_stat)
-        print(lost_time_stat)
+        dictionaryAsArgument['Time Spent Pinging'] = lost_time_stat_status
+
+        # print("SANJAR")
+        # print(internet_provider_status_stat)
+        # print(router_light_up_stat)
+        # print(power_supply_stat)
+        # print(latency_stat)
+        # print(lost_package_stat)
+        # print(lost_time_stat)
         
         
         
         # STOP HERE
         if internet_provider_status_stat == 1:
-            return [100, 1]
+            return [100, dictionaryAsArgument]
         else:
             list.append(0)
 
         
         if(router_light_up_stat == 1):
-            return [100, 1]
+            return [100, dictionaryAsArgument]
         else:
             list.append(0)
         if(power_supply_stat == 1):
-            return [100, 1]
+            return [100,dictionaryAsArgument]
         else:
             list.append(0)
 
         if(latency_stat == "very_bad"):
-            return [100,1]
+            return [100,dictionaryAsArgument]
         elif(latency_stat == "bad"):
             list.append(70)
         elif(latency_stat == "fine"):
@@ -67,7 +92,7 @@ def get_final_stats():
             list.append(0)
 
         if(lost_package_stat >= 0.75):
-            return [100,1]
+            return [100, dictionaryAsArgument]
         elif (lost_package_stat >= 0.45):
             list.append(75)
         elif (lost_package_stat >= 0.2):
@@ -79,7 +104,7 @@ def get_final_stats():
 
 
         if(lost_time_stat == 1):
-            return [100,1]
+            return [100,dictionaryAsArgument]
         elif lost_time_stat == 0.75:
             list.append(75)
         elif lost_time_stat == 0.5:
@@ -94,8 +119,9 @@ def get_final_stats():
         list = sorted(list, reverse = True)
         temp = ((list[0] + list[1]*(0.5)+ list[2]*(0.25)))
         temp  =  ((100) if (temp > 100) else (temp))
-        print("DELETE THIS AFTER")
-        return [temp, temp/100]
+       
+        return [temp, dictionaryAsArgument]
     
     else:
-        return [100,1]
+        #print("")
+        return [100,dictionaryAsArgument]
--- power_supply.py
+++ power_supply.py
@@ -5,8 +5,8 @@ def check_battery():
     if battery is not None:
         percent = battery.percent
         if percent <= 15:
-            return True
-    return False
+            return [True, percent]
+    return [False, percent]
 
 def send_message(message):
     # This function can be customized to send the message in your preferred way,
@@ -16,7 +16,11 @@ def send_message(message):
 
 def get_statistic_powerSupply():
     statistic = 0
-    battery_info = check_battery()
-    if battery_info:
+    battery_info_list = check_battery()
+    battery_info_boolean = battery_info_list[0]
+    battery_percentage = battery_info_list[1]
+    if battery_info_boolean:
         statistic = 1
-    return statistic
+    if statistic == 0:
+        return [0, battery_percentage]
+    return [1, battery_percentage]
