a857784ef6627abd5dc742ea75885bf82fdc46aa
0xKilty <iankilty1@gmail.com>
2024-03-02 22:06:08 -0700

    something

--- api.py
+++ api.py
@@ -1,14 +1,14 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
-from ..equations import get_final_stats
+from equations import get_final_stats
 
 app = Flask(__name__)
 CORS(app, resources={r"/main-value": {"origins": "http://localhost:3000"},
-                    r"/equations": {"origins": "http://localhost:3000"}})
+                     r"/equations": {"origins": "http://localhost:3000"}})
 
 @app.route('/main-value')
 def getMainValue():
-    #main-value = get_final_stats()
+    main_value = get_final_stats()
     return jsonify({
         'main-value': 42
     })
